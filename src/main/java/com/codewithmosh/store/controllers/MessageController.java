e76e1e3c963d5ea57536488253f3c67385b90826
amal <95304547+amal480@users.noreply.github.com>
2025-05-13 18:06:39 +0530

    Dynamic routes

--- src/main/java/com/codewithmosh/store/controllers/MessageController.java
+++ src/main/java/com/codewithmosh/store/controllers/MessageController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageController {
+
     @RequestMapping("/hello")
     public Message sayHello() {
         return new Message("Hello World");
--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -3,16 +3,24 @@ package com.codewithmosh.store.controllers;
 import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/users")
 public class UserController {
     private final UserRepository userRepository;
 
-    @RequestMapping("/users")
+    @RequestMapping()
     public Iterable<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable("id") Long id) {
+        return userRepository.findById(id).orElse(null);
     }
 }
