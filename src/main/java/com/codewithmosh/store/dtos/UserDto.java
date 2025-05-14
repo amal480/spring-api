3315fa983ad63aed9d42f6466179c58f960ba5b7
amal <95304547+amal480@users.noreply.github.com>
2025-05-14 11:40:15 +0530

    Customizing response json

--- src/main/java/com/codewithmosh/store/dtos/UserDto.java
+++ src/main/java/com/codewithmosh/store/dtos/UserDto.java
@@ -1,12 +1,23 @@
 package com.codewithmosh.store.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Getter
 public class UserDto {
+    @JsonProperty("user_id")
     public Long id;
     public String name;
     public String email;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String phoneNumber;
 }
