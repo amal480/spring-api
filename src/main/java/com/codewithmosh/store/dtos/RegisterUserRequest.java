9b9ef677bddffd6e452d3a2253bf479feab26ae7
amal <95304547+amal480@users.noreply.github.com>
2025-05-29 12:57:55 +0530

    Validation

--- src/main/java/com/codewithmosh/store/dtos/RegisterUserRequest.java
+++ src/main/java/com/codewithmosh/store/dtos/RegisterUserRequest.java
@@ -1,10 +1,21 @@
 package com.codewithmosh.store.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RegisterUserRequest {
+    @NotBlank(message = "Name is required") //"", " "
+    @Size(max=255,message="Name must be less than 255 chars")
     private String name;
+
+    @NotBlank
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(min=6, max = 25)
     private String password;
 }
