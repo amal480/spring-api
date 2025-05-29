2090534d4761ba71ac0255cc76a2af532fe22d2c
amal <95304547+amal480@users.noreply.github.com>
2025-05-29 19:51:51 +0530

    Custom Validation

--- src/main/java/com/codewithmosh/store/dtos/RegisterUserRequest.java
+++ src/main/java/com/codewithmosh/store/dtos/RegisterUserRequest.java
@@ -1,5 +1,6 @@
 package com.codewithmosh.store.dtos;
 
+import com.codewithmosh.store.validation.Lowercase;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -13,6 +14,7 @@ public class RegisterUserRequest {
 
     @NotBlank
     @Email
+    @Lowercase()
     private String email;
 
     @NotBlank
